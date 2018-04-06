40f5d8a4001379c03d4cae7f166df47e5c7f61d9
WasabiRaptor <ketchupraptor@gmail.com>
2018-04-06 14:10:12 -0400

    change some formatting

--- quiz.js
+++ quiz.js
@@ -1,9 +1,77 @@
 quizPoints  = [];
 personalityTypes =
-  ["Bold", "Brave", "Calm", "Docile",
-  "Hardy", "Hasty", "Impish", "Jolly",
-  "Lonely", "Naive", "Quiet", "Quirky",
-  "Rash", "Relaxed", "Sassy", "Timid"];
+  ["Bold",    //1
+  "Brave",    //2
+  "Calm",     //3
+  "Docile",   //4
+  "Hardy",    //5
+  "Hasty",    //6
+  "Impish",   //6
+  "Jolly",    //8
+  "Lonely",   //9
+  "Naive",    //10
+  "Quiet",    //11
+  "Quirky",   //12
+  "Rash",     //13
+  "Relaxed",  //14
+  "Sassy",    //15
+  "Timid"];   //16
+
+pokemonTypes =
+  ["Bulbasaur", //1 
+  "Charmander", //2
+  "Squirtle",   //3
+  "Chickorita", //4
+  "Cyndaquil",  //5
+  "Totodile",   //6
+  "Treeko",     //7
+  "Torchic",    //8
+  "Mudkip",     //9
+  "Turtwig",    //10
+  "Chimchar",   //11
+  "Piplup",     //12
+  "Snivy",      //13
+  "Tepig",      //14
+  "Oshawott",   //15
+  "Chespin",    //16
+  "Fennekin",   //17
+  "Froakie",    //18
+  "Rowlet",     //19
+  "Litten",     //20
+  "Popplio",    //21
+  "Pikachu",    //22
+  "Vulpix",     //23
+  "Eevee",      //24
+  "Meowth",     //25
+  "Psyduck",    //26
+  "Machop",     //27
+  "Cubone",     //28
+  "Phanphy",    //29
+  "Skitty",     //30
+  "Shinx",      //31
+  "Munchlax",   //32
+  "Riolu",      //33
+  "Axew",       //34
+  "Magnemite",  //35
+  "Bellsprout", //36
+  "Mareep",     //37
+  "Sentret",    //38
+  "Poochyena",  //39
+  "Aron",       //40
+  "Budew",      //41
+  "Gible",      //42
+  "Klink",      //43
+  "Deino",      //44
+  "Flabebe",    //45
+  "Espurr",     //46
+  "Grubbin",    //47
+  "Rockruff",   //48
+  "Zubat",      //49
+  "Caterpie"];  //50
+
+function pokemonDecide(points) {
+
+}
 
 function nextQuestion(n) {
   if (n < questions.length) {
